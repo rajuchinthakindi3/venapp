f7cf44f975e27c118842faa698374568e90550ff
Kumar <Kumar@Kumar-PC>
2017-06-18 21:39:59 +0530

    first Employee with id, name fields

---
+++ src/com/app/Employee.java
@@ -0,0 +1,29 @@
+package com.app;
+
+public class Employee {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.setId(10);
+		e.setName("raju");
+	}
+
+}
